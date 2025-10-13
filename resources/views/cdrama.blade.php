c6dfdcd1b9740fbce28dd8f67cd356aa611a78ea
cctenshii <SissiWang2003@gmail.com>
2025-10-13 09:34:45 +0200

    readme file + homework ep 3 laracast

---
+++ resources/views/cdrama.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>This is a Cdrama Page</h1>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,27 @@
 <?php
 
+use App\Http\Controllers\CdramaController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/contact', [ContactController::class, 'show'])->name('contact');
+
+Route::get('cdramas/{id}', function (int $id) {
+    return view('cdrama', [
+        'id' => $id
+    ]);
+})->name('cdramas-show');
+
+Route::resource('cdramas', CdramaController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -17,4 +33,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
