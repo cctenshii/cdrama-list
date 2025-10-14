ca2ce70e6b359ffcd584391dc25f904c585753e9
cctenshii <SissiWang2003@gmail.com>
2025-10-14 14:51:36 +0200

    start aan database en het ophalen van de gegevens in de database

--- resources/views/cdramas/index.blade.php
+++ resources/views/cdramas/index.blade.php
@@ -1,4 +1,13 @@
 <x-app-layout>
     <h1>This is the Cdramas Index Page</h1>
+
+    @foreach ($cdramas as $cdrama)
+        <div>
+            <h2>{{ $cdrama->name }}</h2>
+            <p>{{ $cdrama->year }}</p>
+            <p>{{ $cdrama->episodes }}</p>
+            <p>{{ $cdrama->summary }}</p>
+        </div>
+    @endforeach
 </x-app-layout>
 
